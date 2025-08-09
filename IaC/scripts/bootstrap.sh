cc1f992b8d879f51df335c6802ed615ee00336a6
Ravi Pellizzi <r.pellizzi09@gmail.com>
2025-08-09 16:58:21 -0300

    updated

--- IaC/scripts/bootstrap.sh
+++ IaC/scripts/bootstrap.sh
@@ -1,14 +1,17 @@
 # Deploy do Stack de Treinamento Distribuído de Machine Learning com PySpark no Amazon EMR
 # Script de Preparação do Ambiente Python
 
-# Deploy do Stack de Treinamento Distribuído de Machine Learning com PySpark no Amazon EMR
-# Script de Preparação do Ambiente Python
+#!/bin/bash
+set -euxo pipefail
 
-# Instala os pacotes necessários globalmente
-sudo pip3 install --upgrade pip
-sudo pip3 install findspark pendulum boto3 numpy python-dotenv scikit-learn
+# Instala pacotes sem atualizar o pip do sistema
+sudo python3 -m pip install findspark pendulum boto3 numpy python-dotenv scikit-learn
 
-# Cria as pastas
-mkdir $HOME/pipeline
-mkdir $HOME/logs
+# Cria diretórios
+mkdir -p /home/hadoop/pipeline
+mkdir -p /home/hadoop/logs
+
+# Adiciona pipeline ao PYTHONPATH
+echo 'export PYTHONPATH=$PYTHONPATH:/home/hadoop/pipeline' >> /home/hadoop/.bashrc
+
 
